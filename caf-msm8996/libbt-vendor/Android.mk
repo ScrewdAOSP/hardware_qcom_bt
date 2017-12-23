116128bdae9d5f5c39e31258a01ea33fce1dc336
maxwen <max.weninger@gmail.com>
2017-12-23 06:10:39 -0500

    Comment out DFM_OVER_UART and DENABLE_DBG_FLAGS flags
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- caf-msm8996/libbt-vendor/Android.mk
+++ caf-msm8996/libbt-vendor/Android.mk
@@ -31,11 +31,11 @@ LOCAL_SRC_FILES := \
         src/bt_vendor_persist.cpp
 
 #Disable this flag in case if FM over UART support not needed
-LOCAL_CFLAGS := -DFM_OVER_UART
+#LOCAL_CFLAGS := -DFM_OVER_UART
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DPANIC_ON_SOC_CRASH
-LOCAL_CFLAGS += -DENABLE_DBG_FLAGS
+#LOCAL_CFLAGS += -DENABLE_DBG_FLAGS
 endif
 
 LOCAL_C_INCLUDES += \
