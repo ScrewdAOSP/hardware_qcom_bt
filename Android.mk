42b919eb7d00fd853e442fe29f657dfe1b7d2d40
maxwen <max.weninger@gmail.com>
2017-12-23 22:12:27 -0500

    Add TARGET_QCOM_BLUETOOTH_VARIANT to select specific subdir
    - Add caf-msm8996
    
    Change-Id: I7673223c5529f9fe734ddec852fe1eed79a8a142
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+  ifneq ($(filter caf-msm8996,$(TARGET_QCOM_BLUETOOTH_VARIANT)),)
+    include $(call all-named-subdir-makefiles,$(TARGET_QCOM_BLUETOOTH_VARIANT))
+  endif
   ifneq ($(filter msm8x27 msm8226,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8960)
   else ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
